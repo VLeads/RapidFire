a1fb881027b7479924ee1e2a2e2bf803f837da4b
Vishal Kumar <vishal.kumar@benchwork.in>
2022-05-05 14:35:38 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,8 @@
+import "./App.css";
+import logo from "./logo.png";
+
+function App() {
+  return <div className="App">Hello World</div>;
+}
+
+export default App;
---
+++ src/pages/index.js
@@ -0,0 +1,4 @@
+export { Profile } from "./profile/Profile";
+export { Bookmarks } from "./bookmarks/Bookmarks";
+export { Homepage } from "./homepage/Homepage";
+export { Explore } from "./explore/Explore";
