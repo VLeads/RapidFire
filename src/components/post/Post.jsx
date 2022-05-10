7d7a11fa39be3a73084dd33472d3850626690983
Vishal Kumar <vishal.kumar@benchwork.in>
2022-05-10 15:21:07 +0530

    Add homepage

---
+++ src/components/post/Post.jsx
@@ -0,0 +1,32 @@
+import {
+  ChatOutlineIcon,
+  HeartOutlineIcon,
+  ShareIcon,
+} from "assets/icons/icons";
+import React from "react";
+import styles from "./post.module.css";
+
+export const Post = ({ displayName, username, text, avatar }) => {
+  return (
+    <div className={styles.post}>
+      <div className={styles.postAvatar}>
+        <img src={avatar} alt="avatar" className={styles.avatarImg} />
+      </div>
+      <div className={styles.postBody}>
+        <div className={styles.postHeader}>
+          <div className={styles.postHeaderText}>
+            <h3>{displayName}</h3>
+          </div>
+          <div className={styles.postHeaderDescription}>
+            <p>{text}</p>
+          </div>
+        </div>
+        <div className={styles.postFooter}>
+          <ChatOutlineIcon />
+          <HeartOutlineIcon />
+          <ShareIcon />
+        </div>
+      </div>
+    </div>
+  );
+};
--- src/pages/index.js
+++ src/pages/index.js
@@ -2,3 +2,4 @@ export { Profile } from "./profile/Profile";
 export { Bookmarks } from "./bookmarks/Bookmarks";
 export { Homepage } from "./homepage/Homepage";
 export { Explore } from "./explore/Explore";
+export { PageNotFound } from "./page-not-found/PageNotFound";
